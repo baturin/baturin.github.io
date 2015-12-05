087fe97eb43c226241edb9a1d2293661e16db067
Alexey Baturin <alexey.baturin.nsk@gmail.com>
2015-12-05 22:59:36 +0600

    Generate list of files on index page

--- django-site/wvpoi/views.py
+++ django-site/wvpoi/views.py
@@ -1,8 +1,80 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import os
+import re
+from . import settings
+
+class WikivoyageListingsFile(object):
+    def __init__(self, name, language, date, file_format, is_compressed):
+        self._name = name
+        self._language = language
+        self._date = date
+        self._file_format = file_format
+        self._is_compressed = is_compressed
+
+    @staticmethod
+    def parse(filename):
+        m = re.match('^wikivoyage-listings-(\S{2})-([^.]+).(.*?)(.bz2|)$', filename)
+        if not m:
+            return None
+        else:
+            return WikivoyageListingsFile(
+                filename, m.group(1), m.group(2), m.group(3), m.group(4) == '.bz2'
+            )
+
+    @property
+    def language_code(self):
+        return self._language
+
+    @property
+    def language_title(self):
+        return {
+            'en': 'English',
+            'ru': 'Russian',
+            'fr': 'French'
+        }.get(self.language_code, 'Unknown')
+
+    @property
+    def file_format(self):
+        return self._file_format
+
+    @property
+    def file_format_title(self):
+        return {
+            'csv': 'CSV',
+            'kml': 'KML',
+            'gpx': 'GPX',
+            'sql': 'SQL',
+            'osmand.gpx': 'GPX for OsmAnd',
+            'obf': 'OBF (for OsmAnd), "wikivoyage" POI type',
+            'user-defined.obf': 'OBF (for OsmAnd), "user-defined" POI type',
+            'xml': 'XML, "wikivoyage" POI type',
+            'user-defined.xml': 'XML, "user-defined" POI type',
+            'validation-report.html': 'Validation report (HTML)'
+        }.get(self._file_format, self._file_format)
+
+    @property
+    def filename(self):
+        return self._name
+
+    @property
+    def is_latest(self):
+        return self._date == 'latest'
+    
+
+def filter_none_values(lst):
+    return [i for i in lst if i is not None]
+
 
 def index(request):
-    return render(request, 'wvpoi/index.html')
+    all_listings = filter_none_values(
+        WikivoyageListingsFile.parse(filename) 
+        for filename in os.listdir(settings.LISTINGS_DIR)
+    )
+    latest_listings = [l for l in all_listings if l.is_latest]
+    latest_listings = sorted(latest_listings, key=lambda l: (l.language_code, l.file_format))
+    return render(request, 'wvpoi/index.html', {'latest_listings': latest_listings})
+
 
 def tool(request):
     return render(request, 'wvpoi/tool.html')
